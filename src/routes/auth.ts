408c403407a66f5b3bba257562a853ab85356946
Umer Mushtaq <139223033+umermushtaq34@users.noreply.github.com>
2025-07-21 11:53:35 +0500

    reverted working code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,14 @@
 import { Request, Router } from 'express';
 import { AuthController } from '../controllers/authController';
 import { authenticateToken } from '../middlewares/auth';
+<<<<<<< Updated upstream
 import { requireRole } from '../middlewares/requireRole';
 import { uploadFiles } from '../controllers/fileController';
 import { createUploader } from '../middlewares/multerUploader';
+=======
+import { PasswordController } from '../controllers/passwordController';
+import { strictRateLimit } from '../middlewares/rateLimit';
+>>>>>>> Stashed changes
 declare global {
     namespace Express {
         interface Request {
@@ -17,6 +22,7 @@ const router = Router();
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/refresh', AuthController.refreshToken);
+<<<<<<< Updated upstream
 
 // Private routes
 router.get('/profile', authenticateToken, requireRole('admin'), AuthController.getProfile);
@@ -27,5 +33,9 @@ router.put('/change-password', authenticateToken, AuthController.changePassword)
 //     req.uploadFolder = 'uploads';
 //     next();
 // }, createUploader(), uploadFiles);
+=======
+router.post("/forgot-password", strictRateLimit, PasswordController.takeEmail)
+router.put('/change-password', authenticateToken, strictRateLimit, PasswordController.changePassword);
+>>>>>>> Stashed changes
 
 export default router;
