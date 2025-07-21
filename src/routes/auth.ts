eb93be7dfacafb45d3e63a367be9a11bcba0b30a
Umer Mushtaq <139223033+umermushtaq34@users.noreply.github.com>
2025-07-21 11:55:19 +0500

    reverted code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,9 @@
 import { Request, Router } from 'express';
 import { AuthController } from '../controllers/authController';
 import { authenticateToken } from '../middlewares/auth';
-<<<<<<< Updated upstream
 import { requireRole } from '../middlewares/requireRole';
 import { uploadFiles } from '../controllers/fileController';
 import { createUploader } from '../middlewares/multerUploader';
-=======
-import { PasswordController } from '../controllers/passwordController';
-import { strictRateLimit } from '../middlewares/rateLimit';
->>>>>>> Stashed changes
 declare global {
     namespace Express {
         interface Request {
@@ -22,20 +17,15 @@ const router = Router();
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/refresh', AuthController.refreshToken);
-<<<<<<< Updated upstream
 
 // Private routes
 router.get('/profile', authenticateToken, requireRole('admin'), AuthController.getProfile);
 router.put('/profile', authenticateToken, AuthController.updateProfile);
 // router.put('/profile', authenticateToken, AuthController.updateProfile,createUploader(), uploadFiles);
 router.put('/change-password', authenticateToken, AuthController.changePassword);
-// router.post('/upload', (req: Request, res: Express.Response, next: Function) => {
-//     req.uploadFolder = 'uploads';
-//     next();
-// }, createUploader(), uploadFiles);
-=======
-router.post("/forgot-password", strictRateLimit, PasswordController.takeEmail)
-router.put('/change-password', authenticateToken, strictRateLimit, PasswordController.changePassword);
->>>>>>> Stashed changes
+router.post('/upload', (req: Request, res: Express.Response, next: Function) => {
+    req.uploadFolder = 'uploads';
+    next();
+}, createUploader(), uploadFiles);
 
 export default router;
